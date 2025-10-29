662ef27112c5dcc4862b79cb886d4a2d961779b4
Aklra-0303 <2401174@s.asojuku.ac.jp>
2025-10-29 11:33:49 +0900

    データベースの鍵

---
+++ db-connect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER = 'mysql327.phy.lolipop.lan';
+    const DBNAME = 'LAA1607587-ecsitetest';
+    const USER = 'LAA1607587';
+    const PASS = '1123Ec5813';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
