1207e09b97ef8848df591d65d2e4b127bc3de7b5
hina-inAsojuku <2401182@s.asojuku.ac.jp>
2025-11-28 12:34:41 +0900

    写真出てきてくれこの通りだぞなあおい

--- customer/register-input.php
+++ customer/register-input.php
@@ -79,7 +79,7 @@ if (!empty($_SESSION['alert_msg'])) {
   </style>
 </head>
 <body>
-  <img src="logo.png" alt="サイトロゴ">
+  <img src="../image/booknest.png" alt="サイトロゴ">
 
   <?php
   if (isset($_SESSION["alert"])) {
