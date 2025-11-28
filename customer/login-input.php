1ba947e0a1b5515181f73ae964c60edc10bbe8aa
hina-inAsojuku <2401182@s.asojuku.ac.jp>
2025-11-28 11:41:02 +0900

    頼む写真出てくれまじでこの通りだから

--- customer/login-input.php
+++ customer/login-input.php
@@ -17,7 +17,7 @@ session_start();
 <body>
 
     <div style="text-align: center;">
-        <img src="../../image/booknest.png" width="100px" height="100px" alt="books">    
+        <img src="../image/booknest.png" width="100px" height="100px" alt="books">    
     </div>
 
     <form action="login-output.php" method="post" style="width: 300px; margin: 0 auto; padding: 35px; border: 1px solid #ccc; border-radius: 10px;">
