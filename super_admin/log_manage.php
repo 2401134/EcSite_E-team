b849cc64f5a488a01dd068ec50bd2b4fdd59b771
hagioyui <2401166@s.asojuku.ac.jp>
2025-12-03 11:58:37 +0900

    Update log_manage.php

--- super_admin/log_manage.php
+++ super_admin/log_manage.php
@@ -1,4 +1,4 @@
-<?php session_start() ?>
+<?php require 'super_admin_function/logs.php' ?>
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -17,28 +17,24 @@
     <section class="section">
         <div class="container">
             <h1 class="title">ログ管理</h1>
-
+            <form method="GET" class="sort-select field has-addons is-justify-content-flex-end mb-5">
             <!-- ソートセレクトボックス -->
-
-            <div class="sort-select">
-                <div class="field has-addons is-justify-content-flex-end">
-
                     <p class=control>
                         <span class="select is-small mr-4">
-                            <select>
-                                <option>顧客に対する操作</option>
-                                <option>管理者に対する操作</option>
+                            <select name="filter">
+                                <option value="user"<?= $filter=='user'?'selected':'' ?>>顧客に対する操作</option>
+                                <option value="admin"<?= $filter=='admin'?'selected':'' ?>>管理者に対する操作</option>
                             </select>
                         </span>
                     </p>
 
                     <p class=control>
                         <span class="select is-small mr-4">
-                            <select>
-                                <option>日時順</option>
-                                <option>作業者順</option>
-                                <option>操作内容順</option>
-                                <option>作業ID</option>
+                            <select name="sort">
+                                <option value="date"<?= $sort=='date'?'selected':'' ?>>日時順</option>
+                                <option value="user"<?= $sort=='user'?'selected':'' ?>>作業者順</option>
+                                <option value="action"<?= $sort=='action'?'selected':'' ?>>操作内容順</option>
+                                <option value="id"<?= $sort=='id'?'selected':'' ?>>作業ID</option>
                             </select>
                         </span>
                     </p>
@@ -49,8 +45,7 @@
                             <span>ソート</span>
                         </button>
                     </p>
-
-                </div>        
+                </form>    
             </div>
 
             <!-- ログ一覧 -->
@@ -61,41 +56,26 @@
                         <tr>
                             <th>日時</th>
                             <th>作業者</th>
-                            <th>作業対象</th>
+                            <th>操作対象</th>
                             <th>操作内容</th>
                         </tr>
                     </thead>
-
                     <tbody>
+                        <?php foreach($logs as $log){ ?>
                         <tr>
-                            <td>2025-11-10 09:55</td>
-                            <td>森山</td>
-                            <td>顧客(ID)</td>
-                            <td>不適切なコメントの消去</td>
+                            <td><?= htmlspecialchars(date('Y-m-d H:i', strtotime($log['log_date']))) ?></td>
+                            <td><?= htmlspecialchars(displayUserName($log['user_name'],$log['log_id'])) ?></td>
+                            <td><?= htmlspecialchars($log['target']) ?></td>
+                            <td><?= htmlspecialchars($log['action']) ?></td>
                         </tr>
-
-                        <tr>
-                            <td>2025-11-10 09:50</td>
-                            <td>森山</td>
-                            <td>顧客(ID)</td>
-                            <td>ログイン履歴確認</td>
-                        </tr>
-
-                        <tr>
-                            <td>2025-11-10 09:40</td>
-                            <td>森山</td>
-                            <td>顧客(ID)</td>
-                            <td>不審なユーザーのBAN</td>
-                        </tr>
-                    </tbody>                    
+                        <?php } ?>
+                    </tbody>                 
                 </table>
             </div>
-
         </div>
-
         <!--ホームに戻る-->
         <div class="has-text-right mt-5">
-            <form action="super_admin_home.php" method="POST">
+            <form action="admin_home.php" method="POST">
                 <button class="button is-dark">
                     <span class="icon"><i class="fas fa-home"></i></span>
                     <span>ホームに戻る</span>
