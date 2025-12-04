af146f7b98adbad41cfa496036906382b54272dd
2401134 <2401134@s.asojuku.ac.jp>
2025-12-04 19:25:03 +0900

    repair footer

--- customer/footmenu.php
+++ customer/footmenu.php
@@ -1,13 +1,42 @@
+<style>
+  /* ツールチップ共通 */
+  .tooltip {
+    position: relative;
+    cursor: pointer;
+  }
+
+  .tooltip::after {
+    content: attr(data-tooltip);
+    position: absolute;
+    bottom: 130%;
+    left: 50%;
+    transform: translateX(-50%);
+    background: #363636;
+    color: #fff;
+    padding: 6px 10px;
+    border-radius: 6px;
+    white-space: nowrap;
+    opacity: 0;
+    pointer-events: none;
+    transition: opacity .2s ease-in-out;
+    font-size: 0.8rem;
+    z-index: 100;
+  }
+
+  .tooltip:hover::after {
+    opacity: 1;
+  }
+</style>
+
+
 <footer class="footer has-background-grey-lighter p-5">
   <div class="columns is-vcentered">
-
 
     <div class="column is-narrow">
       <figure class="image is-96x96">
         <a href="customer_home.php"><img src="../image/booknest.png" alt="BookNest ロゴ"></a>
       </figure>
     </div>
-
 
     <div class="column">
       <p class="title">ガイドライン</p>
@@ -19,19 +48,26 @@
       </ul>
     </div>
 
-
     <div class="column is-narrow has-text-right">
       <p class="title">SNS share</p>
       <div class="buttons">
-        <a href="#" class="button is-light is-rounded">
+
+        <!-- Twitter -->
+        <a href="#"
+           class="button is-light is-rounded tooltip"
+           data-tooltip="この機能は現在未実装です">
           <span class="icon"><i class="fab fa-twitter"></i></span>
         </a>
-        <a href="#" class="button is-light is-rounded">
+
+        <!-- Instagram -->
+        <a href="#"
+           class="button is-light is-rounded tooltip"
+           data-tooltip="この機能は現在未実装です">
           <span class="icon"><i class="fab fa-instagram"></i></span>
         </a>
+
       </div>
     </div>
 
   </div>
-
 </footer>
