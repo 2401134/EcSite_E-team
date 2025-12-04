6fdd9f6b65a6df4fbbe09e9bf1889b9722b6a0ad
hagioyui <2401166@s.asojuku.ac.jp>
2025-12-04 12:12:46 +0900

    honn

---
+++ customer/customer_function/read.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+require 'db-connect.php';
+
+if (!isset($_POST['book_id'])) {
+    http_response_code(404);
+    echo '<script>history.back();</script>';
+    exit;
+}
+
+$pdo = new PDO($connect, USER, PASS);
+$book_id = $_POST['book_id'];
+
+$sql = $pdo->prepare("SELECT e_book FROM books WHERE book_id = ?");
+$sql->execute([$book_id]);
+$book = $sql->fetch();
+
+if (!$book) {
+    $_SESSION['alert_msg'] = "本が存在しません。";
+    echo '<script>window.location.href = "../read.php";</script>';
+    exit;
+}
+
+$book_pdf = '../' . $book['e_book'];
+
+if (!file_exists($book_pdf)) {
+    $_SESSION['alert_msg'] = "ファイルが存在しません。";
+    echo '<script>window.location.href = "../read.php";</script>';
+    exit;
+}
+
+header("Location: $book_pdf");
+exit;
+?>
