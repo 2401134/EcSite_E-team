2fbfef413744e74412b31d83e2e0c30e036712af
2401134 <2401134@s.asojuku.ac.jp>
2025-12-05 15:53:44 +0900

    repair menu

--- admin/menu.php
+++ admin/menu.php
@@ -1,8 +1,14 @@
 <?php
-// ---- セッションチェック ----
 if (!isset($_SESSION['admin_id'])) {
-    echo "エラー：管理者としてログインしていません。<br>";
-    echo '<a href="Alogin-input.php">ログイン画面へ</a>';
+    http_response_code(404);
+    exit;
+}
+
+if (!isset($_SESSION['super_admin']) || $_SESSION['super_admin'] != 0) {
+    echo '<script>
+          alert("総合管理者の権限がありません");
+          history.back();
+          </script>';
     exit;
 }
 ?>
@@ -10,7 +16,7 @@ if (!isset($_SESSION['admin_id'])) {
 
   
   <figure class="image is-128x128 ml-5">
-    <a href = "admin_home.php"><img src="../uploads/image/booknest.png" width="100px" height="100px"></a>
+    <a href = "super_admin_home.php"><img src="../uploads/image/booknest.png" width="100px" height="100px"></a>
   </figure>
 
 </div>
