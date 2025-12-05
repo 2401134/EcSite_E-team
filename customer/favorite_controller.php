03082183f71a76cc5c45379c78b9ad4502a5cf7b
hagioyui <2401166@s.asojuku.ac.jp>
2025-12-05 11:09:52 +0900

    Update favorite_controller.php

--- customer/favorite_controller.php
+++ customer/favorite_controller.php
@@ -17,7 +17,7 @@ function getFavoriteList($pdo, $user_id) {
                 b.synopsis, 
                 b.sample,
                 b.price,
-                b.book_image
+                b.book_image,
                 p.purchase_date
             FROM favorites f
             JOIN books b ON f.book_id = b.book_id
@@ -25,7 +25,7 @@ function getFavoriteList($pdo, $user_id) {
                 ON f.book_id = p.book_id 
                 AND p.user_id = f.user_id
             WHERE f.user_id = :user_id
-            AND f.favorite_status = 0
+              AND f.favorite_status = 0
             ORDER BY f.favorite_id DESC";
 
     $stmt = $pdo->prepare($sql);
